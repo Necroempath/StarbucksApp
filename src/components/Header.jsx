2a160483e88c3d711ac3c6e4c748519b7fc3eb36
Nate <8bitsodagamer@gmail.com>
2025-11-26 11:33:24 +0400

    header is ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,41 @@
 import { NavLink } from "react-router-dom";
+import { FaShoppingBasket } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header>
-      <nav className="flex items-center 
-      px-5 md:px-6 lg:px-7 xl:px-8
-      py-3 md:py-4 lg:py-5 xl:py-6">
-        <img
-          className="w-10 md:w-12 lg:w-14 xl:w-16"
-          src="https://logo-teka.com/wp-content/uploads/2025/07/starbucks-sign-logo.svg"
-        ></img>
-
-        <NavLink className={"text-transform: uppercase font-bold"}>
-          Menu
+    <header
+      className="flex items-center justify-between border-b border-zinc-400 shadow
+                        px-7 md:px-8 lg:px-9 xl:px-10
+                        py-3 md:py-4 lg:py-5 xl:py-6"
+    >
+      <nav className="flex items-center">
+        <NavLink to="/">
+          <img
+            className="w-8 md:w-10 lg:w-12 xl:w-14
+          mr-7 md:mr-8 lg:mr-9 xl:mr-10"
+            src="https://logo-teka.com/wp-content/uploads/2025/07/starbucks-sign-logo.svg"
+          ></img>
         </NavLink>
-        <NavLink className={"text-transform: uppercase font-bold"}>
-          Rewards
-        </NavLink>
-        <NavLink className={"text-transform: uppercase font-bold"}>
-          Gift cards
-        </NavLink>
+        <div className="flex gap-4 md:gap-5 lg:gap-6 tracking-wide">
+          <NavLink
+            to="/menu"
+            className={({ isActive }) =>
+              `uppercase font-bold ${
+                isActive ? "text-green-700" : "text-black"
+              }`
+            }
+          >
+            Menu
+          </NavLink>
+          <NavLink className={"text-transform: uppercase font-bold"}>
+            Rewards
+          </NavLink>
+          <NavLink className={"text-transform: uppercase font-bold"}>
+            Gift cards
+          </NavLink>
+        </div>
       </nav>
+      <FaShoppingBasket className="text-4xl text-green-700 hover:text-green-900 cursor-pointer" />
     </header>
   );
 };
